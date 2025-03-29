57f3c26e77b9eca26b87143475e27b0147839936
Sabah Baig <sabahbaig.el@gmail.com>
2025-03-29 17:29:28 +0000

    Implemented API logic to fetch data and display on webpage

--- restaurant-data-server/__tests__/server.test.js
+++ restaurant-data-server/__tests__/server.test.js
@@ -1,6 +1,26 @@
 const request = require("supertest");
 const app = require("../server");
-const { describe, test, expect} = require("@jest/globals");
+const { describe, test, expect, beforeEach} = require("@jest/globals");
+
+const mockData = {
+    restaurants: [
+        {
+            id: "102182",
+            name: "itsu - City Road",
+            address: {
+                city: "London",
+                postalCode: "EC1V 1NR"
+            },
+            rating: {
+                starRating: 3.1
+            },
+            cuisines: [
+                { name: "Sushi" },
+                { name: "Asian" }
+            ]
+        }
+    ]
+}
 
 describe("Initialising the server", () => {
     test("The server should start and respond with a health check", async () => {
@@ -18,6 +38,32 @@ describe("Checking CORS configuration", () => {
             .set("Origin", "http://localhost:5173")
 
         expect(response.headers["access-control-allow-origin"]).toBe("http://localhost:5173")
+    })
+})
+
+describe("Implementing the API logic", () => {
+
+    beforeEach(() => {
+        global.fetch = jest.fn();
+    })
+
+    afterEach(() => {
+        jest.restoreAllMocks()
+    })
+
+    test("Calls the API and returns the data", async () => {
+        fetch.mockResolvedValue({
+            json: jest.fn().mockResolvedValue(mockData),
+        })
+
+        const res = await request(app).get("/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
+
+        expect(fetch).toHaveBeenCalledWith(
+            "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF"
+        )
+
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual(mockData)
     })
 })
 
--- restaurant-data-server/server.js
+++ restaurant-data-server/server.js
@@ -12,6 +12,12 @@ app.get("/health", (req, res) => {
     res.send("This server is running.")
 });
 
+app.get("/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF", async (req, res) => {
+    const response = await fetch("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF");
+    const data = await response.json();
+    res.json(data);
+});
+
 if (require.main === module) {
     app.listen(8080, () => console.log("Server started on port 8080"));
 }
