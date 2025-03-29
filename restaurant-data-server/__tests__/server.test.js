f46a4aaeb3e6f8fbaf9594fb97e77d56e52ab7a2
Sabah Baig <sabahbaig.el@gmail.com>
2025-03-29 03:12:28 +0000

    making sure only the frontend can send requests to the backend

--- restaurant-data-server/__tests__/server.test.js
+++ restaurant-data-server/__tests__/server.test.js
@@ -4,11 +4,20 @@ const { describe, test, expect} = require("@jest/globals");
 
 describe("Initialising the server", () => {
     test("The server should start and respond with a health check", async () => {
-        const response = await request(app).get("/health");
+        const response = await request(app)
+            .get("/health");
         expect(response.status).toBe(200);
         expect(response.text).toBe("This server is running.");
     });
 });
 
+describe("Checking CORS configuration", () => {
+    test("Server should only allow requests from http://localhost:5173", async () => {
+        const response = await request(app)
+            .options("/health")
+            .set("Origin", "http://localhost:5173")
 
+        expect(response.headers["access-control-allow-origin"]).toBe("http://localhost:5173")
+    })
+})
 
--- restaurant-data-server/server.js
+++ restaurant-data-server/server.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+}
+
+app.use(cors(corsOptions))
 
 app.get("/health", (req, res) => {
     res.send("This server is running.")
