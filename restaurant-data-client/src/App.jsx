d3df0aab6981fd698825a074a5446da5ea6ee0d3
Sabah Baig <sabahbaig.el@gmail.com>
2025-03-31 13:22:12 +0100

    Webpage can display restaurant names

--- restaurant-data-client/src/App.jsx
+++ restaurant-data-client/src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+
+    const [restaurantData, setRestaurantData] = useState([])
+
+    const fetchAPI = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF")
+            setRestaurantData(response.data.restaurants)
+            console.log(response.data.restaurants)
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchAPI()
+    }, []);
 
   return (
     <>
       <h1>Top 10 Restaurants</h1>
+        {
+            restaurantData.map((restaurant) => (
+                <div key={restaurant.id}>
+                    <p>{restaurant.name}</p>
+                </div>
+            ))
+        }
     </>
   )
 }
--- restaurant-data-client/src/__tests__/App.test.jsx
+++ restaurant-data-client/src/__tests__/App.test.jsx
@@ -1,10 +1,42 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import '@testing-library/jest-dom'
+import { setupServer } from "msw/node"
 import App from '../App'
+import {http, HttpResponse} from "msw";
+
+// Set up a mock server to intercept API requests
+const server = setupServer(
+    http.get("http://localhost:8080/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF", () => {
+        return HttpResponse.json({ restaurants: mockRestaurants})
+    })
+);
+
+const mockRestaurants = [
+    {
+        name: "Al-Baik Pizza",
+    }
+];
+
+// Start and reset the mock server for each test
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe("App Component", () => {
+
     test("renders the web page without crashing", () => {
         render(<App/>)
         expect(screen.getByText("Top 10 Restaurants")).toBeInTheDocument()
+
     })
+
+    test("displays restaurant data", async () => {
+        render(<App />);
+
+        // Wait for the restaurant data to be displayed
+        await waitFor(() => {
+            expect(screen.getByText("Al-Baik Pizza")).toBeInTheDocument();
+        });
+    });
+
 })
